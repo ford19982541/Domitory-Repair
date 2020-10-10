9918b41796916669db9fb5b6a380aa748c9df71a
fordShissanupong <ford19982541@gmail.com>
2020-10-10 14:45:59 +0700

    change class into const

--- RepairApp/screens/Profile.js
+++ RepairApp/screens/Profile.js
@@ -1,29 +1,24 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
-import {
-  ApplicationProvider,
-  Divider,
-  Layout,
-  Button,
-  Text,
-} from "@ui-kitten/components";
-class Profile extends React.Component {
-  render() {
-    const { navigation } = this.props;
-    const navigateProfile = () => {
-      navigation.navigate("Login");
-    };
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <Divider />
-        <Layout
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-          <Text>Profile</Text>
-          <Button onPress={navigateProfile}>LOGOUT</Button>
-        </Layout>
-      </SafeAreaView>
-    );
-  }
+import { Divider, Layout, Button, Text } from "@ui-kitten/components";
+import { AuthContext } from "../contexts/AuthContext";
+
+function Profile({ navigation }) {
+  const { logout } = React.useContext(AuthContext);
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Divider />
+      <Layout
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <Text>Profile</Text>
+        <Button onPress={() => {
+              logout();
+            }}>
+          Sign Out
+        </Button>
+      </Layout>
+    </SafeAreaView>
+  );
 }
 export default Profile;
